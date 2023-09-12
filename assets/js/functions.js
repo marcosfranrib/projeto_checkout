6c053945845c6c65a6b3b0d0a48298844b8e6c30
Marcos Francisco Ribeiro <136476694+marcosfranrib@users.noreply.github.com>
2023-09-12 12:28:54 -0300

    .

---
+++ assets/js/functions.js
@@ -0,0 +1,58 @@
+
+function exibir(valor1,valor2){     
+    valor1.innerText = valor2.value;
+}
+
+function padrao(num) {
+    const regex = /(\d{4})(?=\d)/g;
+    num.value = num.value.replace(/[^0-9]/g, '')
+    num.value = num.value.replace(regex,'$1 ')
+    num.value = num.value.trim()
+}
+
+function alerta(input,alert,exib,content) {
+    if(input.value == ''){
+        input.style.borderColor = 'red'
+        exib.innerHTML = content
+        alert.style.display = 'block'
+
+    } else{
+        input.style.borderColor = '#DEE3E8'
+        exib.innerHTML = input.value;
+        alert.style.display = 'none'
+
+    }
+}
+
+function antiLetra(input) {
+    input.value = input.value.replace(/\D/g,'')
+    
+}
+
+function formatarString(input) {
+    input.value = input.value.replace(/\d/g,'')
+    input.value = input.value.toUpperCase()
+    
+}
+
+function zeroE(input,exib) {
+    if (input.value <= 9 && input.value >= 1 && input.value.length <= 1) {
+        input.value = `0${input.value}`
+        exib.innerHTML = input.value
+
+    } else if(input.value <= 0){
+        input.value = ''
+        exib.innerHTML = '00'
+    }
+}
+
+function verificarEnvio() {
+    let alerta = '';
+    for (let i = 0; i < array.length; i++) {
+        if (array[i].value == '') {
+            array[i].style.borderColor = 'red'
+            alerta = 'Preencha os campos vazios'
+        }
+    }   
+  return alerta;
+}
--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,4 @@
+
 //Atribuição do DOM
     const dgNumero = document.querySelector('#card-number')
     const dgNome = document.querySelector('#card-name')
@@ -15,57 +16,7 @@
     const pCvc = document.querySelector('#p-cvc')
     const submit = document.querySelector('#submit')
     const array = [dgNumero,dgNome,dgMes,dgAno,dgCvc]
-
-//Funções para modelar os padrões de entrada
-   
-    function exibir(valor1,valor2){     
-        valor1.innerText = valor2.value;
-    }
-
-    function padrao(num) {
-        const regex = /(\d{4})(?=\d)/g;
-        num.value = num.value.replace(/[^0-9]/g, '')
-        num.value = num.value.replace(regex,'$1 ')
-        num.value = num.value.trim()
-    }
-
-    function alerta(input,alert,exib,content) {
-        if(input.value == ''){
-            input.style.borderColor = 'red'
-            exib.innerHTML = content
-            alert.style.display = 'block'
-
-        } else{
-            input.style.borderColor = '#DEE3E8'
-            exib.innerHTML = input.value;
-            alert.style.display = 'none'
-
-        }
-    }
-
-    function zeroE(input,exib) {
-        if (input.value <= 9 && input.value >= 1 && input.value.length <= 1) {
-            input.value = `0${input.value}`
-            exib.innerHTML = input.value
-
-        } else if(input.value <= 0){
-            input.value = ''
-            exib.innerHTML = '00'
-        }
-    }
-
-    function verificarEnvio() {
-        let alerta = '';
-        for (let i = 0; i < array.length; i++) {
-            if (array[i].value == '') {
-                array[i].style.borderColor = 'red'
-                alerta = 'Preencha os campos vazios'
-            }
-        }   
-      return alerta;
-    }
-    
-
+ 
 // Eventos de entrada de input
    
     dgNumero.addEventListener('input', () =>{
@@ -76,8 +27,7 @@
     })
 
     dgNome.addEventListener('input',()=>{
-        dgNome.value = dgNome.value.replace(/[0-9]/g,'')
-        dgNome.value = dgNome.value.toUpperCase()
+        formatarString(dgNome)
         exibir(nome,dgNome)
         alerta(dgNome,pNome,nome,'seu nome aqui')  
         
@@ -87,6 +37,7 @@
         if (dgMes.value > 12) {
             dgMes.value = 12
         }
+        antiLetra(dgMes)
         exibir(mes,dgMes)
         alerta(dgMes,pData,mes,'00')  
         
@@ -99,6 +50,7 @@
 
 
     dgAno.addEventListener('input',()=>{
+        antiLetra(dgAno)
         exibir(ano,dgAno)
         alerta(dgAno,pData,ano,'00') 
     })
@@ -108,6 +60,7 @@
     })
 
     dgCvc.addEventListener('input',()=>{
+        antiLetra(dgCvc)
         exibir(cvc,dgCvc)
         alerta(dgCvc,pCvc,cvc,'000') 
         
@@ -120,7 +73,6 @@
             alert(verificar)
 
        }else{
-           
             window.location.href = "http://www.devmedia.com.br/guia/javascript/34372";
             console.log('passei do prevent');
        }
